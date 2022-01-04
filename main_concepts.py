df264ca199b82bcd2970e526d4aeee9e9944ef41
vis7 <vishvajeetramanuj95@gmail.com>
2022-01-04 18:47:06 +0530

    streamlit tutorial completed

--- main_concepts.py
+++ main_concepts.py
@@ -1,6 +1,14 @@
 import pandas as pd
 import streamlit as st
 import numpy as np
+import time
 
-df = np.random.randn(10,20)
-st.write(df)
+latest_iteration = st.empty()
+bar = st.progress(0)
+
+for i in range(100):
+    latest_iteration.text(f"Iteration {i+1}")
+    bar.progress(i+1)
+    time.sleep(0.1)
+
+st.write("... and now we\'re done!")
